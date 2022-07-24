98ae2c482a8f4106da174fee8c78329a304872c9
margaret manyeki <maggiemanyeki6@gmail.com>
2022-07-24 23:03:18 +0300

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
-function App() {
-  return (
+function App(){
+  const [show,setShow] = useState (true)
+  const [poems, setPoems] =useState ([]);
+  
+  
+  useEffect (()=>{
+    fetch("http://localhost:8004/poems")
+    .then ((res) => res.json())
+    .then ((data) => setPoems(data));
+  },[]);
+  
+
+  function handleClick(){
+    console.log(show)
+    setShow(!show)
+  }
+  function updatePoems(poems){
+    setPoems([...poems,poem])
+  }
+  return(
     <div className="app">
-      <div className="sidebar">
-        <button>Show/hide new poem form</button>
-        {true ? <NewPoemForm /> : null}
-      </div>
-      <PoemsContainer />
+    <div className="sidebar">
+      <button onClick={handleHideClick}>{show ? 'Hide Form':'Show Form'}</button>
+      {show ? <NewPoemForm updatePoems ={updatePoems} /> : null}
     </div>
+    <PoemsContainer poems ={poems} setPoems = {setPoems}/>
+  </div>
   );
-}
+  }
 
 export default App;
